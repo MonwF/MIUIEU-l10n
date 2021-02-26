60812fd37a26dedc8a78c650db53026cccdd5fd4
xiongjie <xiongjie@corp.netease.com>
2021-02-26 22:13:19 +0800

    fix: 修复日历

--- app/src/main/java/name/monwf/miuil10n/MainModule.java
+++ app/src/main/java/name/monwf/miuil10n/MainModule.java
@@ -1,34 +1,38 @@
 package name.monwf.miuil10n;
 
+import de.robv.android.xposed.IXposedHookInitPackageResources;
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.IXposedHookZygoteInit;
 
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+import name.monwf.miuil10n.utils.ResourceHooks;
 
 
 public class MainModule implements IXposedHookZygoteInit, IXposedHookLoadPackage {
-
-//	public static ResourceHooks resHooks;
+	private ResourceHooks resHooks;
 
 	public void initZygote(StartupParam startParam) {
-//		resHooks = new ResourceHooks();
-
-
+		resHooks = new ResourceHooks();
 	}
 
+	@Override
 	public void handleLoadPackage(final LoadPackageParam lpparam) {
 		String pkg = lpparam.packageName;
 
 		if (pkg.equals("com.android.contacts")
 				|| pkg.equals("com.miui.weather2")
 				|| pkg.equals("com.android.mms")
-				|| pkg.equals("com.android.calendar")
 				|| pkg.equals("com.android.deskclock")
 				|| pkg.equals("com.miui.yellowpage")
 		) {
 			Class<?> classBuild = XposedHelpers.findClass("miui.os.Build", lpparam.classLoader);
 			XposedHelpers.setStaticBooleanField(classBuild, "IS_INTERNATIONAL_BUILD",false);
+		}
+		else if (pkg.equals("com.android.calendar")) {
+			resHooks.setObjectReplacement(pkg, "bool", "is_greater_china", true);
+			resHooks.setObjectReplacement(pkg, "bool", "is_mainland_china", true);
 		}
 	}
 }
